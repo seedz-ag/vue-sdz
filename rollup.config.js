9a60e69decc0c7f13f1ac61aa910666c5567829a
viniazvd <vinicius.azevedo@seed.ag>
2021-07-21 16:37:37 -0300

    :construction: wip

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,42 @@
-const vue = require('rollup-plugin-vue')
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+
+import babel from 'rollup-plugin-babel'
+
+import vue from 'rollup-plugin-vue'
+
+import scss from 'rollup-plugin-scss'
+import postcss from 'postcss'
+import autoprefixer from 'autoprefixer'
+
+import { terser } from 'rollup-plugin-terser'
 
 const config = {
   external: ['vue'],
 
-  plugins: [ vue() ],
-
-  input: 'src/index.ts',
+  input: 'src/index.js',
 
   output: [
     { format: 'cjs', file: 'dist/bundle.cjs.js' },
     { format: 'esm', file: 'dist/bundle.esm.js' }
-  ]
+  ],
+
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({ exclude: 'node_modules/**' }),
+    vue(),
+    // vue({ css: false }),
+    scss({
+      output: 'dist/css/',
+      processor: css => postcss([autoprefixer])
+        .process(css)
+        .then(result => result.css)
+    }),
+    terser()
+  ],
+
+  external: ['examples']
 }
 
 export default config
