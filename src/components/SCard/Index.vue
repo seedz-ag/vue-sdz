d012c07efc8a360dd659a059780e4f69e7435563
viniazvd <vinicius.azevedo@seed.ag>
2021-07-28 15:24:44 -0300

    :zap: eslint tests

--- src/components/SCard/Index.vue
+++ src/components/SCard/Index.vue
@@ -1,12 +1,61 @@
 <template>
-  <div class="c-card">
-    card
-  </div>
+  <component
+    :is="'componentX'"
+
+    class="foo"
+    a-custom-prop="value"
+
+    z-prop="Z"
+    boolean-prop
+
+    :another-custom-prop="value"
+    :blue-color="false"
+    :class="bar"
+
+    @[c]="functionCall"
+    @change="functionCall"
+    @click="functionCall"
+    @input="functionCall"
+
+    v-text="textContent"
+  />
 </template>
 
 <script>
 export default {
-  name: 'SCard'
+  name: 'SCard',
+
+  functional: false,
+
+  components: {},
+
+  mixins: [],
+
+  inheritAttrs: false,
+
+  propsData: {},
+
+  props: {
+    a: {
+      type: String,
+      required: true
+    }
+  },
+
+  data () {
+    return {}
+  },
+  watch: {},
+  renderError () {},
+  beforeCreate () {},
+  created () {},
+  beforeMount () {},
+  mounted () {},
+  computed: {},
+  methods: {},
+  render: () => ({}),
+  beforeUnmount () {},
+  unmounted () {}
 }
 </script>
 
