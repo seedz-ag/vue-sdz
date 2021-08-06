dfbaed85ada331d2a122925ee837742df6657407
vinicius <viniazvd@gmail.com>
2021-08-06 16:39:01 -0300

    feat: :sparkles: (scope1) desc
    long desc

--- src/components/SCard/index.test.js
+++ src/components/SCard/index.test.js
@@ -4,7 +4,7 @@ import SCard from './Index.vue'
 describe('SCard', () => {
   it('test example', async () => {
     const wrapper = shallowMount (SCard)
-    console.log('dasdasdasddadsdasdasdasddasddasddasddasddasdasdasd')
+    console.log('dasdasdasddadsdasdasdasddasddasddasddasddasdasdasddasd')
 
     expect(wrapper.html()).toContain('card')
   })
