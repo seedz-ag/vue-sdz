031dfbd3f9b90e9f04da75bdcc645d0333b07c37
vinicius <viniazvd@gmail.com>
2021-08-06 16:16:56 -0300

    fix: das

--- src/components/SCard/index.test.js
+++ src/components/SCard/index.test.js
@@ -4,7 +4,7 @@ import SCard from './Index.vue'
 describe('SCard', () => {
   it('test example', async () => {
     const wrapper = shallowMount (SCard)
-    console.log('12adasddasdadsadsadasdsdasddadasdsadsadsda')
+    console.log('dasdasdasddads')
 
     expect(wrapper.html()).toContain('card')
   })
