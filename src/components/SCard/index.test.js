43ccbfe0b2590060dbe5e5ba7d534453677cfe4a
vinicius <viniazvd@gmail.com>
2021-08-06 17:23:50 -0300

    perf(dasd): :zap: dsadsaasfasfsa
    fasfas

--- src/components/SCard/index.test.js
+++ src/components/SCard/index.test.js
@@ -4,7 +4,7 @@ import SCard from './Index.vue'
 describe('SCard', () => {
   it('test example', async () => {
     const wrapper = shallowMount (SCard)
-    console.log('dasdasdasddadsdasdaddasdasdasddasddsadasdadasdsdasddasddasddasddasddasdasdasddasddasddasddasda')
+    console.log('dasdasdasddadsdasdaddasdasdasddasddsaddasddasdsaasdadasdsdasddasddasddasddasddasdasdasddasddasddasddasda')
 
     expect(wrapper.html()).toContain('card')
   })
