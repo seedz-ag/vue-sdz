588af7b37a992d443b48cc7553e2602d68329417
vinicius <viniazvd@gmail.com>
2021-08-06 18:16:50 -0300

    fix(scopee): :bug: desc teste
    long descccc

--- src/components/SCard/index.test.js
+++ src/components/SCard/index.test.js
@@ -4,7 +4,7 @@ import SCard from './Index.vue'
 describe('SCard', () => {
   it('test example', async () => {
     const wrapper = shallowMount (SCard)
-    console.log('dasdasdasddadsdasdaddasdasdasddasddsaddasddasdsaasdadasdsdasddasddasddasddasddasdasdasddasddasddasddasda')
+    console.log('dadasddasddasdsadasdasdsdaddsd')
 
     expect(wrapper.html()).toContain('card')
   })
