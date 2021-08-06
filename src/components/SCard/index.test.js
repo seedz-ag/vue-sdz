8630465eb6a85576e0a9eca6393354d9f86fd392
vinicius <viniazvd@gmail.com>
2021-08-06 14:36:15 -0300

    feat: dsadas

--- src/components/SCard/index.test.js
+++ src/components/SCard/index.test.js
@@ -4,7 +4,7 @@ import SCard from './Index.vue'
 describe('SCard', () => {
   it('test example', async () => {
     const wrapper = shallowMount (SCard)
-    console.log('12adasddasdadsadsa')
+    console.log('12adasddasdadsadsadasds')
 
     expect(wrapper.html()).toContain('card')
   })
