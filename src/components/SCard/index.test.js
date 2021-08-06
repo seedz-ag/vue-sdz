36b0d13f5e4799ee1b87e8b6d113db5ddbdb716f
vinicius <viniazvd@gmail.com>
2021-08-06 17:05:00 -0300

    style(scopeee): :art: desccccc
    long desccc

--- src/components/SCard/index.test.js
+++ src/components/SCard/index.test.js
@@ -4,7 +4,7 @@ import SCard from './Index.vue'
 describe('SCard', () => {
   it('test example', async () => {
     const wrapper = shallowMount (SCard)
-    console.log('dasdasdasddadsdasdaddasdasasdadasdsdasddasddasddasddasddasdasdasddasddasddasddasda')
+    console.log('dasdasdasddadsdasdaddasdasdasddasddsadasdadasdsdasddasddasddasddasddasdasdasddasddasddasddasda')
 
     expect(wrapper.html()).toContain('card')
   })
