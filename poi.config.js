85e3048c0ba6db4a620515d1165574e922b3a527
vinicius <viniazvd@gmail.com>
2021-08-11 11:15:44 -0300

    config: :wrench: config

--- poi.config.js
+++ poi.config.js
@@ -1,8 +1,25 @@
+// const path = require('path')
+
 module.exports = {
   entry: 'examples/main.js',
 
+  // publicFolder : 'static',
+
   output: {
-    // dir: 'docs',
+    dir: 'docs',
+    minimize: true,
+    // sourceMap: false,
+    fileNames: {
+      js: 'poi.js'
+    },
+    html: {
+      filename: 'index.html'
+    },
+    moduleName: 'SeedzAg'
     // publicUrl: 'dominio-seedz.com.br'
-  }
+  },
+
+  // chainWebpack(config) {
+  //   config.resolve.alias.set('@seedz-ag/vue-sdz$', path.resolve('src/index.js'))
+  // }
 }
