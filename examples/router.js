c1598ca624e4b8196fdff878a320c7e0f0b7bfd7
vinicius <viniazvd@gmail.com>
2021-09-09 14:49:40 -0300

    init: :tada: init

--- examples/router.js
+++ examples/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import GettingStarted from './GettingStarted.vue'
+
 import ComponentList from './components/Index.vue'
 import ComponentsRoute from './components/routes.js'
 
@@ -8,6 +10,11 @@ import APIList from './api/Index.vue'
 import APIsRoute from './api/routes.js'
 
 const routes = [
+  {
+    path: '/getting-started',
+    name: 'getting-started',
+    component: GettingStarted
+  },
   {
     path: '/components',
     name: 'components',
