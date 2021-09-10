026de9107f2a761f15cc7efa3a6388fe75b8e88c
vinicius <viniazvd@gmail.com>
2021-09-10 11:40:16 -0300

    init: :tada: init

---
+++ examples/utils/DocumentationTable.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="documentation-table">
+    <div class="title">{{ title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DocumentationTable',
+
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.documentation-table {
+
+}
+</style>
