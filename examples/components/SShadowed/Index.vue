a1683487913115ae7b72a9e3a56066ee1cbb622f
vinicius <viniazvd@gmail.com>
2021-09-14 10:54:09 -0300

    init: :tada: init

---
+++ examples/components/SShadowed/Index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="s-shadowed-example">
+    <s-shadowed class="example-1">
+      <div class="wrapper">
+        AAA <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        BBB <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        CC <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        DDD <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        EEE <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+        aaaaaaaa <br>
+      </div>
+    </s-shadowed>
+  </div>
+</template>
+
+<script>
+import SShadowed from '../../../src/components/SShadowed/Index.vue'
+
+export default {
+  components: { SShadowed }
+}
+</script>
+
+<style lang="scss">
+.s-shadowed-example {
+  padding: 50px;
+  margin: 0 auto;
+
+  & > .example-1 {
+    & > .wrapper {
+      height: 400px;
+      overflow-y: scroll;
+      background-color: gray;
+    }
+  }
+}
+</style>
