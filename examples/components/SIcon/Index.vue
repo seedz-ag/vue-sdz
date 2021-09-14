265600ab38feffe74e81829ce5d39957b012712e
vinicius <viniazvd@gmail.com>
2021-09-14 14:03:05 -0300

    init: :tada: init

---
+++ examples/components/SIcon/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="s-icon-example">
+    <s-icon image="plus" size="30" />
+
+    <s-icon :image="image" size="40" @click.native="changeImage" />
+  </div>
+</template>
+
+<script>
+import SIcon from '../../../src/components/SIcon/Index.vue'
+
+export default {
+  components: { SIcon },
+
+  data () {
+    return {
+      image: 'heart'
+    }
+  },
+
+  methods: {
+    changeImage () {
+      this.image = 'plus'
+    }
+  }
+}
+</script>
