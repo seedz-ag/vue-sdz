f4ebe2d8c1a9eaa02357f789a02d9529f378bd28
vinicius <viniazvd@gmail.com>
2021-10-04 15:42:05 -0300

    config: :wrench: build config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
 
 export default defineConfig({
+  root: path.join(__dirname, '/'),
+
+  build: {
+    outDir: path.join(__dirname, 'docs')
+  },
+
   plugins: [  createVuePlugin() ],
 
   server: { port: 8080 }
