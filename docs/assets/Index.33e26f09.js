a446b48462fd9aaf58652bdbd6f583baadf9fb80
vinicius <viniazvd@gmail.com>
2021-10-04 15:54:24 -0300

    docs: :pencil: add docs

---
+++ docs/assets/Index.33e26f09.js
@@ -0,0 +1,1 @@
+import{n as e,S as t}from"./index.3689a1fc.js";import"./vendor.86f0ea26.js";const n={};var s=e({components:{SSidebar:t}},(function(){var e=this.$createElement,t=this._self._c||e;return t("div",{staticClass:"s-sidebar-example"},[t("s-sidebar")],1)}),[],!1,r,null,null,null);function r(e){for(let t in n)this[t]=n[t]}var a=function(){return s.exports}();export{a as default};
---
+++ docs/assets/Index.b30ed866.js
@@ -0,0 +1,1 @@
+import{n as t}from"./index.3689a1fc.js";import"./vendor.86f0ea26.js";const n={};var r=t({},(function(){var t=this,n=t.$createElement;return(t._self._c||n)("div",{staticClass:"s-card-example"},[t._v(" card api ")])}),[],!1,e,null,null,null);function e(t){for(let r in n)this[r]=n[r]}var a=function(){return r.exports}();export{a as default};
