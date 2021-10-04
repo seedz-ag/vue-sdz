6e1813981cbd4ba4cae0b3c1e9dc206e842369d8
vinicius <viniazvd@gmail.com>
2021-10-04 16:01:33 -0300

    docs: :pencil: doc files

--- docs/assets/Index.23fdea25.js
+++
@@ -1,1 +0,0 @@
-import{n as t}from"./index.3689a1fc.js";import"./vendor.86f0ea26.js";const n={};var e=t({},(function(){var t=this,n=t.$createElement;return(t._self._c||n)("div",{staticClass:"s-modal-example"},[t._v(" modal api ")])}),[],!1,a,null,null,null);function a(t){for(let e in n)this[e]=n[e]}var r=function(){return e.exports}();export{r as default};
--- docs/assets/Index.a91459c1.js
+++
@@ -1,1 +0,0 @@
-import{n as t}from"./index.3689a1fc.js";import"./vendor.86f0ea26.js";const n={};var e=t({},(function(){var t=this,n=t.$createElement;return(t._self._c||n)("div",{staticClass:"s-button-example"},[t._v(" btn api ")])}),[],!1,r,null,null,null);function r(t){for(let e in n)this[e]=n[e]}var a=function(){return e.exports}();export{a as default};
