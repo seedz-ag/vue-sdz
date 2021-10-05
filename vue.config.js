a0a8a01af9ee56e8d3defbd2f5a5d1017800255f
vinicius <viniazvd@gmail.com>
2021-10-05 10:06:49 -0300

    config: :wrench: config github pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://seedz-ag.github.io/vue-sdz/'
-    : '/vue-sdz/'
+  publicPath: 'https://seedz-ag.github.io/vue-sdz/'
 }
