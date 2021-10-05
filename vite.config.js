67b606ff3a56420d9948efafc824dfa34cf830fd
vinicius <viniazvd@gmail.com>
2021-10-05 10:02:52 -0300

    config: :wrench: config github pages

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     outDir: path.join(__dirname, 'docs')
   },
 
-  base: '/vue-sdz/',
+  base: 'https://seedz-ag.github.io/vue-sdz/',
 
   plugins: [  createVuePlugin() ],
 
