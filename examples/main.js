7b19a5c27ac8225d330de99f12aff9453c96dd6f
vinicius <viniazvd@gmail.com>
2021-10-06 14:42:08 -0300

    feature: :sparkles: add vuex

--- examples/main.js
+++ examples/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
+import store from './store'
 import router from './router'
 
 import Example from './Index.vue'
 
+import { VueCoeFeedback } from 'vue-coe-feedback'
+
 import '../src/styles/_index.scss'
 
 Vue.config.productionTip = false
+Vue.use(VueCoeFeedback, store, { delay: 3000 })
 
 export default new Vue({
+  store,
   router,
   render: h => h(Example),
 }).$mount('#app')
