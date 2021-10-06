b0e2c3e09a9b9639024a2285beb244dd75c3df38
vinicius <viniazvd@gmail.com>
2021-10-06 15:08:08 -0300

    docs: :pencil: update doc files

--- docs/assets/Index.98179972.js
+++
@@ -1,1 +0,0 @@
-import{n as t}from"./index.72d943e5.js";import"./vendor.86f0ea26.js";const e={};var n=t({},(function(){var t=this,e=t.$createElement;return(t._self._c||e)("div",{staticClass:"s-modal-example"},[t._v(" modal api ")])}),[],!1,r,null,null,null);function r(t){for(let n in e)this[n]=e[n]}var a=function(){return n.exports}();export{a as default};
