6403ab62f0d502aeccab99c9fa4516d1ec918164
viniazvd <viniazvd@gmail.com>
2021-10-18 17:29:59 -0300

    style: :art: exporting all components

--- src/index.js
+++ src/index.js
@@ -11,11 +11,22 @@ if (typeof window !== 'undefined' && window.Vue) install(window.Vue)
 export default install
 
 // Register components individually
-export { default as SCard } from './components/SCard/Index.vue'
-export { default as SModal } from './components/SModal/Index.vue'
 export { default as SButton } from './components/SButton/Index.vue'
+export { default as SCard } from './components/SCard/Index.vue'
+export { default as SCheckbox } from './components/SCheckbox/Index.vue'
+export { default as SCollapsible } from './components/SCollapsible/Index.vue'
+export { default as SFeedbacks } from './components/SFeedbacks/Index.vue'
+export { default as SIcon } from './components/SIcon/Index.vue'
+export { default as SInput } from './components/SInput/Index.vue'
+export { default as SInputContainer } from './components/SInputContainer/Index.vue'
+export { default as SLink } from './components/SLink/Index.vue'
+export { default as SLoader } from './components/SLoader/Index.vue'
+export { default as SModal } from './components/SModal/Index.vue'
 export { default as SOverlay } from './components/SOverlay/Index.vue'
+export { default as SSelect } from './components/SSelect/Index.vue'
+export { default as SShadowed } from './components/SShadowed/Index.vue'
 export { default as SSidebar } from './components/SSidebar/Index.vue'
+export { default as STable } from './components/STable/Index.vue'
 
 
 // Register layouts individually
