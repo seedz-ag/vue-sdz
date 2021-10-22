6f392ddb9891e4711fd63edc7140883704346b6d
viniazvd <viniazvd@gmail.com>
2021-10-22 15:11:02 -0300

    docs: :pencil: doc files

---
+++ docs/assets/Index.cf193303.js
@@ -0,0 +1,1 @@
+import{n as t}from"./index.8758d5b1.js";import"./vendor.5affd582.js";const n={};var e=t({},(function(){var t=this,n=t.$createElement;return(t._self._c||n)("div",{staticClass:"s-modal-example"},[t._v(" modal api ")])}),[],!1,r,null,null,null);function r(t){for(let e in n)this[e]=n[e]}var a=function(){return e.exports}();export{a as default};
