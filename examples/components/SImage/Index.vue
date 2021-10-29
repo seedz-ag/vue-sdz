64465acd5e0089be5befbb91911b74095d6a7eee
viniazvd <viniazvd@gmail.com>
2021-10-29 09:44:48 -0300

    init: :tada: init

---
+++ examples/components/SImage/Index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="s-image-example">
+    <s-image
+      :src="src"
+      :duration="100"
+      fallback="https://i.ytimg.com/vi/Yt9t9e9gmmw/maxresdefault.jpg"
+    />
+
+    <button @click="changeImage('chaves')">chaves</button>
+    <button @click="changeImage('seumadruga')">seu madruga</button>
+    <button @click="changeImage('nhonho')">nhonho</button>
+    <button @click="changeImage('victao')">victao</button>
+  </div>
+</template>
+
+<script>
+import SImage from '../../../src/components/SImage/Index.vue'
+
+export default {
+  components: { SImage },
+
+  data () {
+    return {
+      src: ''
+    }
+  },
+
+  created () {
+    this.changeImage('chaves')
+  },
+
+  methods: {
+    changeImage (image) {
+      this.src = new URL(`../../../src/assets/examples/${image}.jpg`, import.meta.url).href
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.s-image-example {
+  margin: 0 auto;
+
+  & > .s-image { width: 250px; }
+}
+</style>
---
+++ src/components/SImage/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <vue-coe-image class="s-image" :duration="duration" v-bind="$attrs" v-on="$listeners" />
+</template>
+
+<script>
+import VueCoeImage from 'vue-coe-image'
+import 'vue-coe-image/dist/vue-coe-image.css'
+
+export default {
+  name: 'SImage',
+
+  components: { VueCoeImage },
+
+  props: {
+    duration: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./src/styles/_index.scss";
+
+.s-image {
+
+}
+</style>
