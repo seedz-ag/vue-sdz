24fef2c999442804a0184fa7ffffb3ba09fd8a50
viniazvd <viniazvd@gmail.com>
2021-11-05 16:07:44 -0300

    docs: :pencil: doc files

--- docs/assets/Index.53a3fd3f.js
+++
@@ -1,1 +0,0 @@
-import{n as t}from"./index.e7f21670.js";import"./vendor.5affd582.js";const n={};var e=t({},(function(){var t=this,n=t.$createElement;return(t._self._c||n)("div",{staticClass:"s-modal-example"},[t._v(" modal api ")])}),[],!1,r,null,null,null);function r(t){for(let e in n)this[e]=n[e]}var a=function(){return e.exports}();export{a as default};
