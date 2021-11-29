0bee7c5a7a861f79fcb182a93cc276bcb1618bec
laiseassis <32445474+laiseassis@users.noreply.github.com>
2021-11-29 20:48:05 -0300

    Adicionando Textos

--- examples/components/SRadiobox/Index.vue
+++ examples/components/SRadiobox/Index.vue
@@ -2,6 +2,16 @@
   <div class="s-radiobox-example">
     <h1 class="h1">Radiobox</h1>
 
+    <p class="subtitle">
+      Input Radio Button tem como objetivo possibilitar que a pessoa usuária escolha uma opção dentro da interface.
+      <br><br>
+      Deve ser usado quando é permitido selecionar apenas uma única opção em uma lista, principalmente em cenários onde estão expostos todas as opções disponíveis.
+      <br><br>
+      Podem ser utilizados em alguns componentes como: Listas, Formulários e Tabelas.
+      <br><br>
+      Não deve ser usado se mais de uma opção puder ser selecionada, nesse cenário devemos utilizar o checkbox.
+    </p>
+
     <h2 class="h2">Default</h2>
     <s-box>
       <s-radiobox v-model="data0" label="Default Radiobox" />
