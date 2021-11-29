ed83a055388634d501ef53d9e5e3b3ac4c05be6f
laiseassis <32445474+laiseassis@users.noreply.github.com>
2021-11-29 20:43:26 -0300

    Adicionando texto ao spinner

--- examples/components/SLoader/Index.vue
+++ examples/components/SLoader/Index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="s-loader-example flex-line">
-    <h1 class="h1">Loader</h1>
+    <h1 class="h1">Spinner</h1>
+    <p class="subtitle">
+      Spinners fornecem uma indicação visual de que uma ação está sendo processada, aguardando um curso de mudança ou um resultado.
+    </p>
+    <h2 class="h2">Quando usar</h2> 
+    <p class="subtitle">
+      Deve ser usado substituindo o label dos botões primários ao salvar ou validar dados. Ao salvar ou validar dados, o botão deve ficar no estado disabled e respeitando a cor neutra mais clara configurada neste documento.
+    </p>
     <s-box class="flex-inline flex-center">
       <s-loader />
     </s-box>
@@ -22,4 +29,8 @@ export default {
   justify-content: center;
 }
 .s-box{width:100%;}
+
+.h2{
+  margin-top: 1rem;
+}
 </style>
