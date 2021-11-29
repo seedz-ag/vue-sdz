09c499f88fbf7bcb6d46d5d995e4da65b9690a1c
laiseassis <32445474+laiseassis@users.noreply.github.com>
2021-11-29 20:46:11 -0300

    Adição do texto do switch

--- examples/components/SSwitch/Index.vue
+++ examples/components/SSwitch/Index.vue
@@ -2,18 +2,23 @@
   <div class="s-switch-example">
     <h1 class="h1">Switch</h1>
 
+    <p class="subtitle">
+      Switch tem como objetivo possibilitar a pessoa usuária habilitar ou desabilitar algo como um alternador binário de "on/off".
+    </p>
+
+    <h2 class="h2">Regras de aplicação</h2> 
+
+    <p class="subtitle">
+      Podem ser utilizados para habilitar ou desabilitar configurações como "receber ou não" push notifications;
+      <br><br>
+      Não deve ser usado em cenários onde é interessante a pessoa usuária conseguir confirmar as opções selecionadas antes das ações serem aplicadas. Nesses casos deve-se utilizar o checkbox.
+    </p>
+
     <h2 class="h2">Default</h2>
     <s-box>
       <s-switch v-model="data0" label="Default Switch" />
       <br>
       <s-switch v-model="data2" label="Checked Switch"/>
-    </s-box>
-
-    <h2 class="h2">Stats</h2>
-    <s-box>
-      <s-switch label="Positive" v-model="data1" />
-      <br>
-      <s-switch label="Negative" negative v-model="data2" />
     </s-box>
 
     <h2 class="h2">Disabled</h2>
