1688526a2716354c99342dfd7f61e2c65ac04993
viniazvd <viniazvd@gmail.com>
2021-12-15 17:11:33 -0300

    feature: :sparkles: init

---
+++ examples/components/SPagination/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="s-pagination-example">
+    <h1 class="h1">Pagination</h1>
+
+    <s-box>
+      <s-pagination
+        total-page="10"
+        :page="page"
+        @page="value => page = value"
+      />
+    </s-box>
+  </div>
+</template>
+
+<script>
+import SBox from '../../commons/box.vue'
+import SPagination from '../../../src/components/SPagination/Index.vue'
+
+export default {
+  components: { SPagination, SBox },
+
+  data () {
+    return {
+      page: 1
+    }
+  },
+
+  methods: {
+    setPage (page) {
+      this.page = page
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.s-pagination-example {}
+</style>
