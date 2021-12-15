ad889c0dedb579ed3721467ec9d091f38b660de5
viniazvd <viniazvd@gmail.com>
2021-12-15 17:13:22 -0300

    docs: :pencil: doc files

--- docs/assets/Index.7e07abfe.js
+++
@@ -1,1 +0,0 @@
-import{n as t,b as e}from"./index.1f769140.js";import"./vendor.5affd582.js";const n={};var s=t({components:{SSidebar:e}},(function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"s-sidebar-example"},[e("s-sidebar")],1)}),[],!1,r,null,null,null);function r(t){for(let e in n)this[e]=n[e]}var a=function(){return s.exports}();export{a as default};
