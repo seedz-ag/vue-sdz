1bf41e43ba804bbda8190d278d58fc418c252b09
laiseassis <32445474+laiseassis@users.noreply.github.com>
2021-12-23 14:53:39 -0300

    Avatar pt1

---
+++ examples/components/SAvatar/Index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <s-title class="h1" title="Avatar" />
+
+    <p class="subtitle">
+      Avatar é uma representação em miniatura de uma pessoa usuária, para representar uma organização, use o componente "brand".
+    </p>
+
+    <s-title class="h2" title="Quando usar" />
+
+    <p class="subtitle">
+      Esse componente pode ser usado em lugares onde existe o upload de uma foto da pessoa usuária;
+    </p>
+
+    <p class="subtitle">
+      Pode ser usado em contextos onde essa imagem é fixa, como em um card de depoimento.
+    </p>
+
+    <s-box class="flex-inline flex-center">
+      <s-avatar :size="p" />
+      <s-avatar />
+      <s-avatar :size="g" />
+    </s-box>
+  </div>
+</template>
+
+<script>
+import SAvatar from '../../../src/components/SAvatar/Index.vue'
+import SBox from '../../commons/box.vue'
+import STitle from '../../commons/title.vue'
+
+export default {
+  components: { SAvatar, SBox, STitle },
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/SAvatar/Index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="s-avatar">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SAvatar'
+
+  // props: {
+  //   value: {
+  //     type: Number,
+  //     required: true,
+  //     validator: val => val >= 0 && val <= 100
+  //   }
+  // }
+}
+</script>
+
+<style lang="scss">
+@import "./src/styles/_index.scss";
+
+.s-avatar {
+  border-radius: 100px;
+  display: flex;
+  background: color(primary, base);
+  height: 40px;
+  width: 40px;
+
+  & > .icon{
+
+  }
+}
+</style>
