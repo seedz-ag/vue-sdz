42fa706b700dd4c5e953b2a11a28cf50fd27db47
laiseassis <32445474+laiseassis@users.noreply.github.com>
2021-12-28 16:07:52 -0300

    404

---
+++ docs/assets/Index.e91b0a91.js
@@ -0,0 +1,1 @@
+import{n as e,e as t}from"./index.6de2ca96.js";import"./vendor.8161f9ef.js";const n={};var s=e({components:{SSidebar:t}},(function(){var e=this.$createElement,t=this._self._c||e;return t("div",{staticClass:"s-sidebar-example"},[t("s-sidebar")],1)}),[],!1,r,null,null,null);function r(e){for(let t in n)this[t]=n[t]}var a=function(){return s.exports}();export{a as default};
