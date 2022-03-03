ed9dfacef23becabdc92982b24def982eb321f42
viniazvd <viniazvd@gmail.com>
2022-03-03 17:28:23 -0300

    config: :wrench: fix build configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,60 +1,32 @@
 import vue from 'rollup-plugin-vue'
-
 import scss from 'rollup-plugin-scss'
-
-// import postcss from 'postcss'
-// import autoprefixer from 'autoprefixer'
-
 import commonjs from '@rollup/plugin-commonjs'
 
-// import { terser } from 'rollup-plugin-terser'
-
-import { babel } from '@rollup/plugin-babel'
-
 const config = {
-  input: 'src/index.js',
-
   inlineDynamicImports: true,
 
+  external: ['vue'],
+
+  input: 'src/index.js',
+
   output: [
-    // {
-    //   format: 'cjs',
-    //   file: 'dist/js/build.cjs.js',
-    //   plugins: [terser()]
-    // },
+    {
+      format: 'cjs',
+      file: 'dist/js/bundle.cjs.js'
+    },
     {
       format: 'esm',
-      file: 'dist/js/build.esm.js',
-      // plugins: [terser()]
-    },
-    // {
-    //   format: 'iife',
-    //   file: 'dist/js/build.min.js',
-    //   globals: { vue: 'Vue' },
-    //   name: 'bundleee',
-    //   plugins: [terser()]
-    // }
+      file: 'dist/js/bundle.esm.js'
+    }
   ],
 
   plugins: [
-    scss({
-      // eslint-disable-next-line quotes
-      // prefix: `@import "src/styles/index.scss";`,
-      output: 'dist/css/build.css',
-      // outputStyle: 'compressed',
-      // processor: () => postcss([autoprefixer()])
-    }),
-
-    // terser(),
+    commonjs(),
 
     vue({ css: false }),
 
-    commonjs(),
-
-    babel({ babelHelpers: 'bundled' })
-  ],
-
-  // external: ['vue']
+    scss({ output: 'dist/css/bundle.css' })
+  ]
 }
 
 export default config
