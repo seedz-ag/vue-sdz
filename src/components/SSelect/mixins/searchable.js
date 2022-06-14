d56db771c738eec344e99e2593405641b975a392
viniazvd <viniazvd@gmail.com>
2022-06-14 13:02:08 -0300

    refactoring: :recycle: refactor

--- src/components/SSelect/mixins/searchable.js
+++ src/components/SSelect/mixins/searchable.js
@@ -5,18 +5,8 @@ const searchable = {
     }
   },
 
-  computed: {
-    searchableValue () {
-      if (this.searchQuery) return this.searchQuery
-      if (this.focused && !this.searchQuery) return ''
-      if ((!this.value || !this.value?.length) && !this.searchQuery) return [ this.placeholder ]
-
-      return this.multiple ? '' : this.value
-    }
-  },
-
   methods: {
-    search ({ target: { value } }) {
+    onSearch ({ target: { value } }) {
       this.isOpened = true
       this.focused = false
 
