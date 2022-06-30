cd1769d4d1b6977c10afad0fdc2af231d27b725f
viniazvd <viniazvd@gmail.com>
2022-06-30 16:49:38 -0300

    docs: :pencil: doc assets

---
+++ docs/assets/Index.afa09fbd.js
@@ -0,0 +1,19 @@
+import{n as o,f as d,S as i,a as l,P as a}from"./index.c4d66450.js";import u from"./Index.7ecb838c.js";import"./vendor.0f0055ec.js";import"./Index.31a51fe2.js";import"./clickOutside.84c04b54.js";import"./Index.0aa49698.js";var r=function(){var s=this,e=s.$createElement,t=s._self._c||e;return t("div",{staticClass:"s-button-example"},[t("s-title",{attrs:{size:"title-1"}},[s._v("Buttons")]),t("source-code",{attrs:{file:"SButtonExample",comp:"SButton"}},[s._v("Example")]),t("s-title",{attrs:{size:"title-2"}},[s._v("Stats")]),t("s-box",[t("div",{staticClass:"flex-inline"},[t("s-button",{attrs:{primary:""}},[s._v("primary")]),t("s-button",{attrs:{success:""}},[s._v("primary")]),t("s-button",{attrs:{error:""}},[s._v("primary")]),t("s-button",{attrs:{loading:""}},[s._v("loading")]),t("s-button",{attrs:{disabled:""}},[s._v("disabled")]),t("s-button",{attrs:{link:""}},[s._v("link button")])],1),t("br"),t("br"),t("div",{staticClass:"flex-line"},[t("s-button",{attrs:{"full-width":""}},[s._v("full-width")])],1)]),t("pre-code",{attrs:{code:s.code1}}),t("s-title",{attrs:{size:"title-2"}},[s._v("Sizes")]),t("s-box",{staticClass:"flex-inline"},[t("s-button",{attrs:{small:""}},[s._v("small")]),t("s-button",{attrs:{default:""}},[s._v("default")]),t("s-button",{attrs:{large:""}},[s._v("large")])],1),t("pre-code",{attrs:{code:s.code2}}),t("s-title",{attrs:{size:"title-2"}},[s._v("Outlined")]),t("s-box",{staticClass:"flex-inline"},[t("s-button",{attrs:{outlined:""}},[s._v("outlined")]),t("s-button",{attrs:{outlined:"",disabled:""}},[s._v("outlined disabled")]),t("s-button",{attrs:{outlined:"",icon:"sdz-check"}},[s._v("outlined icon")]),t("s-button",{attrs:{outlined:"",icon:"sdz-check",disabled:""}},[s._v("outlined disabled icon")])],1),t("pre-code",{attrs:{code:s.code3}}),t("s-title",{attrs:{size:"title-2"}},[s._v("Rounded")]),t("s-box",{staticClass:"flex-inline"},[t("s-button",{attrs:{rounded:""}},[s._v("outlined")]),t("s-button",{attrs:{rounded:"",disabled:""}},[s._v("rounded disabled")]),t("s-button",{attrs:{rounded:"",icon:"sdz-check"}},[s._v("rounded icon")]),t("s-button",{attrs:{rounded:"",icon:"sdz-check",disabled:""}},[s._v("rounded disabled icon")])],1),t("pre-code",{attrs:{code:s.code4}}),t("s-title",{attrs:{size:"title-2"}},[s._v("Transparent + outlined + white")]),t("s-box",{staticClass:"transparent-box"},[t("s-button",{attrs:{transparent:"",outlined:"",white:""}},[s._v("icon")])],1)],1)},c=[];const b={components:{SButton:d,SBox:i,STitle:l,SourceCode:u,PreCode:a},data(){return{code1:`<div class="flex-inline">
+        <s-button primary>primary</s-button>
+        <s-button grey>Secondary</s-button>
+        <s-button loading>loading</s-button>
+        <s-button disabled>disabled</s-button>
+        <s-button link>link button</s-button>
+      </div>
+
+      <div class="flex-line">
+        <s-button full-width>full-width</s-button>
+      </div>`,code2:`<s-button small>small</s-button>
+      <s-button default>default</s-button>
+      <s-button large>large</s-button>`,code3:`<s-button outlined>outlined</s-button>
+      <s-button outlined disabled>outlined disabled</s-button>
+      <s-button outlined icon="sdz-check">outlined icon</s-button>
+      <s-button outlined icon="sdz-check" disabled>outlined disabled icon</s-button>`,code4:`<s-button rounded>outlined</s-button>
+      <s-button rounded disabled>rounded disabled</s-button>
+      <s-button rounded icon="sdz-check">rounded icon</s-button>
+      <s-button rounded icon="sdz-check" disabled>rounded disabled icon</s-button>`}}},n={};var _=o(b,r,c,!1,v,null,null,null);function v(s){for(let e in n)this[e]=n[e]}var k=function(){return _.exports}();export{k as default};
