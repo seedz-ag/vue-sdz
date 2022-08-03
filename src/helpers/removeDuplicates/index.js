250b939b0f7f7f5590d173e282a68afe82c78b81
viniazvd <viniazvd@gmail.com>
2022-08-03 16:56:08 -0300

    remove: :fire: remove

--- src/helpers/removeDuplicates/index.js
+++
@@ -1,9 +0,0 @@
-export default (arr, prop) => {
-  const digest = arr.reduce((acc, el) => {
-    if (!acc.has(el[prop])) acc.set(el[prop], el)
-
-    return acc
-  }, new Map())
-
-  return [...digest.values()]
-}
