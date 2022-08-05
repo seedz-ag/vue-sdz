010bf272171ace7513d9c4db75dc2cf64e7f8c4d
viniazvd <viniazvd@gmail.com>
2022-08-05 15:02:10 -0300

    config: :wrench: add/config alias and build

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,42 @@
+import path from 'path'
 import vue from 'rollup-plugin-vue'
 import scss from 'rollup-plugin-scss'
+import alias from '@rollup/plugin-alias'
+import copy from 'rollup-plugin-copy-assets'
 import commonjs from '@rollup/plugin-commonjs'
 
 const config = {
-  inlineDynamicImports: true,
+  // inlineDynamicImports: true,
 
   external: ['vue'],
 
   input: 'src/index.js',
 
   output: [
-    {
-      format: 'cjs',
-      file: 'dist/js/bundle.cjs.js'
-    },
+    // {
+    //   format: 'cjs',
+    //   file: 'dist/js/bundle.cjs.js'
+    // },
     {
       format: 'esm',
-      file: 'dist/js/bundle.esm.js'
+      // file: 'dist/js/bundle.esm.js',
+      dir: 'dist',
+      preserveModules: true,
+      preserveModulesRoot: 'src'
     }
   ],
 
-  plugins: [ commonjs(), vue(), scss() ]
+  plugins: [
+    alias({
+      entries: {
+        ['@']: path.resolve(__dirname, 'src')
+      }
+    }),
+    commonjs(),
+    vue(),
+    scss(),
+    copy({ assets: ['src/assets'] })
+  ]
 }
 
 export default config
