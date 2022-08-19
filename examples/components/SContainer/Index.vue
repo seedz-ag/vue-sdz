063a97e483ff60c02c1f41e756541707f52d8843
viniazvd <viniazvd@gmail.com>
2022-08-19 14:57:10 -0300

    feature: :sparkles: init

---
+++ examples/components/SContainer/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="s-container-example">
+    <s-container>
+      <div>teste sdz</div>
+    </s-container>
+  </div>
+</template>
+
+<script>
+import SContainer from '../../../src/components/SContainer/Index.vue'
+
+export default {
+  name: 'SContainerExample',
+
+  components: { SContainer }
+}
+</script>
+
+<style lang="scss">
+.s-container-example {
+
+}
+</style>
