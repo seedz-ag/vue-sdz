25e340de20a38a16c8cf5db3ea50bb823e8d095b
viniazvd <viniazvd@gmail.com>
2022-09-28 19:26:43 -0300

    init: :tada: init

---
+++ examples/components/SDatePicker/Index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="s-date-picker-example">
+    simple
+    <s-date-picker v-model="date" />
+
+    show-disabled-days
+    <s-date-picker show-disabled-days v-model="date" />
+
+    range
+    <s-date-picker is-range v-model="date" />
+  </div>
+</template>
+
+<script>
+import SDatePicker from '../../../src/components/SDatePicker/Index.vue'
+
+export default {
+  name: 'SDatePickerExample',
+
+  components: { SDatePicker },
+
+  data () {
+    return {
+      date: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.s-date-picker-example {
+
+}
+</style>
