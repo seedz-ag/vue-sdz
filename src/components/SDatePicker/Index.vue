45080fe9d1624eb6e6f927e50ded907e98fb80d2
viniazvd <viniazvd@gmail.com>
2022-09-28 19:26:05 -0300

    feature: :sparkles: init

---
+++ src/components/SDatePicker/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <calendar class="s-date-picker" v-bind="$attrs" v-on="$listeners" />
+</template>
+
+<script>
+import Calendar from 'vue-coe-calendar'
+
+export default {
+  name: 'SDatePicker',
+
+  components: { Calendar }
+}
+</script>
+
+<style lang="scss">
+.s-date-picker {
+  height: auto !important;
+}
+</style>
